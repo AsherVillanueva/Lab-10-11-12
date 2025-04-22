afa0d304b23b3646387ef9cb9cff6c8e492d7231
AsherVillanueva <bavillanueva@paterostechnologicalcollege.edu.ph>
2025-04-22 09:15:54 +0800

    Add files via upload

---
+++ Lab 12/DatabaseConnection.java
@@ -0,0 +1,15 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/school?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
